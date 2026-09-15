324a07519a818655c6390c05f4dd2003dac6e592
Yaromir291 <yaanonim@duck.com>
2026-09-15 18:55:52 +0300

    Revert "Update from task f9885175-8b5c-4684-a7b5-76a64786f557"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -100,21 +100,14 @@ namespace Yaromir_Firewall_FINAL1
 
         private void UpdateLanguage()
         {
-            var lang = LanguageService.Instance;
-            
             LangButton.Content = _isRussian ? "🇷🇺" : "🇬🇧";
-            LangButton.ToolTip = lang.GetResource(_isRussian ? "MainWindow_LangButton_ToolTip_RU" : "MainWindow_LangButton_ToolTip_EN");
+            LangButton.ToolTip = _isRussian ? "Русский" : "English";
 
-            OpenMonitorButton.Content = lang.GetResource("MainWindow_OpenMonitorButton");
-            WhiteListButton.Content = lang.GetResource("MainWindow_WhiteListButton");
-            BlackListButton.Content = lang.GetResource("MainWindow_BlackListButton");
+            OpenMonitorButton.Content = _isRussian ? "Открыть мониторинг" : "Open Monitor";
+            WhiteListButton.Content = _isRussian ? "Белый список" : "Whitelist";
+            BlackListButton.Content = _isRussian ? "Чёрный список" : "Blacklist";
 
-            ThemeButton.ToolTip = lang.GetResource("MainWindow_ThemeButton_ToolTip");
-            AboutButton.ToolTip = lang.GetResource("MainWindow_AboutButton_ToolTip");
-            MinimizeButton.Content = lang.GetResource("MainWindow_MinimizeButton");
-            
             UpdateStatus();
-            LicenseText.Text = lang.GetResource("MainWindow_LicenseText");
         }
 
         public void UpdateLanguageFromService()
--- MonitorWindow.xaml.cs
+++ MonitorWindow.xaml.cs
@@ -33,11 +33,6 @@ namespace Yaromir_Firewall_FINAL1
             BackButton.ToolTip = lang.GetResource("MonitorWindow_BackButton_ToolTip");
             RefreshRateLabel.Text = lang.GetResource("MonitorWindow_RefreshRateLabel");
             
-            // Обновление элементов ComboBox
-            FastOption.Content = lang.GetResource("MonitorWindow_RefreshRate_Fast");
-            ModerateOption.Content = lang.GetResource("MonitorWindow_RefreshRate_Moderate");
-            SlowOption.Content = lang.GetResource("MonitorWindow_RefreshRate_Slow");
-            
             // Обновление заголовков колонок
             if (ConnectionsGrid.Columns.Count >= 5)
             {
@@ -46,7 +41,6 @@ namespace Yaromir_Firewall_FINAL1
                 ConnectionsGrid.Columns[2].Header = lang.GetResource("MonitorWindow_Column_LocalPort");
                 ConnectionsGrid.Columns[3].Header = lang.GetResource("MonitorWindow_Column_RemoteAddress");
                 ConnectionsGrid.Columns[4].Header = lang.GetResource("MonitorWindow_Column_Protocol");
-                ConnectionsGrid.Columns[5].Header = lang.GetResource("MonitorWindow_Column_Status");
             }
         }
 
